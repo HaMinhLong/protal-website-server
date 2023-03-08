70fc0021969b82b9077f6ca13ce1fb8ac25fbd0a
“HaMinhLong” <haminhlong3@gmail.com>
2023-03-08 11:19:42 +0700

    update cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000', 'https://protal-website.herokuapp.com'];
+  const allowedOrigins = ['http://localhost:3000', 'https://protal-website.herokuapp.com', 'https://cms.naru.software'];
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
@@ -99,5 +99,5 @@ require('./app/routes/website/collection.routes')(app);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
